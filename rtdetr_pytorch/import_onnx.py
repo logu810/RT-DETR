527189645755342a4eaf6392ed76aa695ca0f7d9
logu810 <810logu@gmail.com>
2024-11-29 16:41:58 +0530

    size added

--- rtdetr_pytorch/import_onnx.py
+++ rtdetr_pytorch/import_onnx.py
@@ -4,6 +4,7 @@ from torchvision.transforms import ToTensor
 from src.data.coco.coco_dataset import mscoco_category2name, mscoco_category2label, mscoco_label2category
 
 
+size = torch.tensor([[640, 640]])
 # print(onnx.helper.printable_graph(mm.graph))
 
 # Load the original image without resizing
