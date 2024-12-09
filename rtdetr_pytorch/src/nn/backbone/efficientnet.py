bd5fe594d6cf243bf859857a9e1eb8456bf7a49e
logu810 <810logu@gmail.com>
2024-12-09 19:16:14 +0000

    forward

--- rtdetr_pytorch/src/nn/backbone/efficientnet.py
+++ rtdetr_pytorch/src/nn/backbone/efficientnet.py
@@ -14,10 +14,10 @@ class EfficientNet(nn.Module):
         self.model = EfficientNetModel.from_pretrained("google/efficientnet-b7")
         self.return_idx = return_idx
 
-
     def forward(self, x):
-        
-        outputs = self.model(x, output_hidden_states = True)
-        x = outputs.hidden_states[2:5]
+        outputs = self.model(x, output_hidden_states=True)
+        x = [outputs.hidden_states[i] for i in self.return_idx]  # Return layers at return_idx
+        # Add debugging print
+        print([x.shape for x in [outputs.hidden_states[i] for i in self.return_idx]])
 
         return x
