6bd3b30b56da9b3abd5828ecd1dec0b3f1ddfd41
logu810 <810logu@gmail.com>
2024-12-10 04:26:01 +0000

    final

--- rtdetr_pytorch/src/nn/backbone/mobilenet.py
+++ rtdetr_pytorch/src/nn/backbone/mobilenet.py
@@ -19,20 +19,12 @@ class MobileNet(nn.Module):
         # Forward pass through the model
         outputs = self.model(x, output_hidden_states=True)
         
-        # Debugging: Print the shapes of all hidden states
-        print("Hidden states from the RegNet model:")
-        for i, hidden_state in enumerate(outputs.hidden_states):
-            print(f"Hidden state {i} shape: {hidden_state.shape}")
-        
+
         # Select the relevant hidden states based on return_idx
         x = outputs.hidden_states[2:3]  # For 224 channels
         x += outputs.hidden_states[5:6]  # For 384 channels
         x += outputs.hidden_states[12:13]  # For 640 channels
 
         
-        # Debugging: Print the selected feature shapes
-        print("Selected hidden states (indices 2:5):")
-        for i, hidden_state in enumerate(x):
-            print(f"Selected Hidden state {i} shape: {hidden_state.shape}")
-        
+
         return x
